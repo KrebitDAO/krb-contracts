b7c7c53b419c2308b8c3250649f9ae8c9526d0bd
Alejandro Rios <arios@livevox.com>
2022-09-26 15:45:00 -0500

    testing zkEVM

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,14 @@
 require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
-
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.12",
+    version: "0.8.16",
     settings: {
       optimizer: {
         enabled: true,
@@ -30,6 +31,9 @@ module.exports = {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: { mnemonic: process.env.MNEMONIC },
     },
+    hardhat: {
+      zksync: true,
+    },
   },
   etherscan: {
     // Your API key for Etherscan
@@ -39,5 +43,22 @@ module.exports = {
       xdai: "api-key",
       polygonMumbai: process.env.POLYGONSCAN_API_KEY,
     },
+  },
+  zksolc: {
+    version: "1.1.5",
+    compilerSource: "docker",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+      experimental: {
+        dockerImage: "matterlabs/zksolc",
+        tag: "v1.1.5",
+      },
+    },
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
+    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
   },
 };
